8b3ce7da54b5ea9df85751b19fbd35d58a120de3
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-20 01:02:51 +0700

    [PHAN-04] Jsx va ham map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const a1 = "nho";
 
-import React, { Component } from 'react';
-
-function One() {
-  <div>
-     <h2> so 1 tap 1 </h2>
-     <h3> so 1 tap 2 </h3>
-     </div>
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3>toi ten la : {a1} </h3>
+      </div>
+    )
+  }
 }
+
+//vd ve ham map
+const so = [1,2,3,4,5,6];
+const so2 = so.map((x) => x*2);
+class App2 extends Component {
+  render() {
+    return (
+      <div>
+         { so2 }
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <One/>
-      </header>
-    </div>
+        <header className="App=header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <App2/>
+        </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload. 
+          </p>
+      </div>
     );
   }
 }
-
 
 export default App;
