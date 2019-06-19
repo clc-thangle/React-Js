def96ecb2c1eddee234913c40cf4ffcc7122bbd2
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-19 23:49:58 +0700

    [PHAN-02] Dinh nghia component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import React, { Component } from 'react';
-
 function One() {
+  return (
   <div>
-     <h2> so 1 tap 1 </h2>
-     <h3> so 1 tap 2 </h3>
-     </div>
+    <h1>So 1 tap 1</h1>
+    <h5>So 1 tap cuoi</h5>
+  </div> );
 }
 
-class App extends Component {
+var Two = function()
+{
+  return (
+    <div>
+    <h3> Cach so 2 </h3>
+    </div>
+  );
+}
+
+var Three = () =>(
+  <div>
+    <h5> Cach so 3 </h5>
+    </div>
+);
+
+class Four extends Component {
+  render() {
+    return (
+      <div>
+         <button> React JS component so 4 </button>
+      </div>
+    );
+  }
+}
+class Five extends Component {
+  render() {
+    return (
+      <div>
+         <button> React JS component so 5 </button>
+      </div>
+    );
+  }
+}
+
+class App  extends Component{
   render() {
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <One/>
+        
+        <One></One>
+        <Two/>
+        <Three/>
+        <hr/>
+        <Four/>
+        <Five/>
       </header>
     </div>
     );
