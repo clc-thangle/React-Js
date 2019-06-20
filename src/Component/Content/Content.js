546df4ed740b0603ca98b0bb4c87126650ce2731
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-20 13:07:39 +0700

    [PHAN-07] Khai niem state

--- src/Component/Content/Content.js
+++ src/Component/Content/Content.js
@@ -4,12 +4,45 @@ class Content extends Component {
 
     constructor(props) {
         super(props);
-        
+        this.state = {
+            trangThai:0
+        }
     }
 
     thongbao = () => {alert('cach xu ly tuong tac trong react js');}
     thongbao2 = () => {alert('thong bao so 2');}
 
+    renderButton = () => { // la rang a// dung viet theo kieu arrow func á// da rua de e thu lai 
+        return (
+        <div className="row">
+                <div className="btn btn-group">
+                <div className="btn btn-info" onClick={this.thongbao}> Edit </div>
+                <div className="btn btn-warning"onClick={this.thongbao2}> Remove </div>
+                </div>
+        </div>
+        );
+    }
+    renderForm  = () => {
+        return (
+        <div classname="row">
+                <div className="form-group">
+                <input type="text" name="ten" className="form-control" />
+                <div className="btn btn-block btn-danger">Save</div>
+                </div>
+        </div>
+        );
+    }
+
+    displayCheck  = () => {
+        if(this.state.trangThai===0)
+        {
+            return this.renderButton();
+        }
+        else
+            return this.renderForm();
+
+    }
+    
     render() {
         return (
                     <div className="col-lg-4">
@@ -25,16 +58,8 @@ class Content extends Component {
                                     <p>
                                         {this.props.trichdan}
                                     </p>
-                                    <div>
-                                        <div className="row">
-                                          <div className="btn btn-group">
-                                            <div className="btn btn-info" onClick={this.thongbao}> Edit </div>
-                                            <div className="btn btn-warning"onClick={this.thongbao2}> Remove </div>
-                                          </div>
-                                        </div>
-                                        <hr />
-                                      </div>
-                                      
+                                    {this.displayCheck()}
+                                    <hr/>
                                 </div>
                             </div>
                         </div>
