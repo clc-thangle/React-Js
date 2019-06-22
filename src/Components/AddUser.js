1718a2bca57d8c328036982e1da32d8cb6e5cb6a
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-22 18:43:30 +0700

    [PHAN-21] Xu ly cac tuong tac co ban

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
 
 class AddUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trangThaiChinhSua : false
+        }
+    }
+
+    thayDoiTrangThai = () => {
+        this.setState({
+            trangThaiChinhSua: !this.state.trangThaiChinhSua
+        })
+    }
+
+    hienThiNut = () => {
+        if (this.state.trangThaiChinhSua===true) {
+            return <div className="btn btn-block btn-outline-secondary" onClick={()=>this.thayDoiTrangThai()}>Đóng lại</div>;
+        }
+        else {
+            return <div className="btn btn-block btn-outline-info" onClick={()=>this.thayDoiTrangThai()}>Thêm mới</div>;
+        }
+    }
+    
+    hienThiForm = () => {
+        if (this.state.trangThaiChinhSua===true) {
+            return (
+                <div className="card mb-3 mt-2" style={{maxWidth: '18rem'}}>
+                    <div className="card-header">Thêm mới user vào hệ thống</div>
+                    <div className="card-body">
+                        <div className="form-group">
+                            <input type="text" className="form-control" aria-describedby="helpId" placeholder="Tên User" />
+                        </div>
+                         <div className="form-group">
+                            <input type="text" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" />
+                        </div>
+                        <div className="form-group">
+                            <select className="custom-select" required>
+                                <option value>Chọn quyền mặc định</option>
+                                <option value={1}>Admin</option>
+                                <option value={2}>Moderator</option>
+                                <option value={3}>Normal</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <div className="btn btn-block btn-primary">Thêm mới</div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        
+    }
+
     render() {
         return (
-            <div className="col-3" style={{color: 'white'}}>
-  <div >
-    <div className="card text-white bg-primary mb-3">
-      <div className="card-header">Thêm mới</div>
-      <div className="card-body">
-        <div className="form-group">
-          
-          <input type="text" className="form-control"  placeholder="Tên user" />
-        </div>
-        <div className="form-group">
-         
-          <input type="text" className="form-control"  placeholder="Điện thoại" />
-        </div>
-        <hr />
-        <div className="form-group">
-          <select className="custom-select" required>
-            <option value>Chọn quyền mặc định</option>
-            <option value={1}>Admin</option>
-            <option value={2}>Modrator</option>
-            <option value={3}>Nomal</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <div className="btn btn-block btn-primary">
-            Thêm mới
+            <div className="col-3">
+            <div>
+                {this.hienThiNut()}
+                {this.hienThiForm()}
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
+          
         );
     }
 }
