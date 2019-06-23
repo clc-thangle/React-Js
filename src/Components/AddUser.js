67334ba04b6be7604fa08c2a4228e9c2fef6b7c1
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-24 00:49:19 +0700

    [PHAN-25] 072 Lay du lieu thanh vien duoc them moi bang su kien onChange

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 
 class AddUser extends Component {
 
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        const tel = event.target.value;
+        const Permission = event.target.value;
+        console.log(name);
+        console.log(value);
+        
+        
+    }
+
     kiemTraTrangThai  = () => {
         if(this.props.hienThiForm === true)
         {
@@ -11,13 +22,13 @@ class AddUser extends Component {
                     <div className="card-header">Thêm mới user vào hệ thống</div>
                     <div className="card-body">
                         <div className="form-group">
-                            <input type="text" className="form-control" aria-describedby="helpId" placeholder="Tên User" />
+                            <input type="text" onChange={(event) => this.isChange(event)} name="name" className="form-control" aria-describedby="helpId" placeholder="Tên User" />
                         </div>
                          <div className="form-group">
-                            <input type="text" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" />
+                            <input type="text" onChange={(event) => this.isChange(event)} name="tel" className="form-control" aria-describedby="helpId" placeholder="Điện thoại" />
                         </div>
                         <div className="form-group">
-                            <select className="custom-select" required>
+                            <select className="custom-select" onChange={(event) => this.isChange(event)} name="Permission" required>
                                 <option value>Chọn quyền mặc định</option>
                                 <option value={1}>Admin</option>
                                 <option value={2}>Moderator</option>
