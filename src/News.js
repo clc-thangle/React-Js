a2e8a43953e330b3f16e14f8aef5c18b1c6b77f1
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-25 18:13:42 +0700

    [PHAN-37] Tao ung dung ket hop React_Redux

---
+++ src/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class News extends Component {
+    render() {
+        return (
+            <div>
+                <h2>day la component new</h2>   
+            </div>
+        );
+    }
+}
+
+export default News;        
