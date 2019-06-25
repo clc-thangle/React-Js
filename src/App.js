d8769da3ddf49a79a7583fe9592df3a26cfc15d4
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-25 17:44:48 +0700

    [PHAN-36] 105 Ham subcribe trong store

--- src/App.js
+++ src/App.js
@@ -18,31 +18,36 @@ class App extends Component {
       {
         case "CHANGE_EDIT_STATUS":
           return {...state,editStatus:!state.editStatus}
-          break;
+
         case "ADD_NEW":
           return {...state,num:[...state.num,action.newItem]}
-          break;
+
         case "DELETE":
-          return {...state,num:state.num.filter((value,i) => i != action.index)}
+          return {...state,num:state.num.filter((value,i) => i !== action.index)}
         default:
             return state;
-          break;
+          
       }
       
     }
     var store1 = redux.createStore(reducer1); 
+
+    store1.subscribe(() => {
+      console.log(JSON.stringify(store1.getState()));
+      
+    })
     store1.dispatch({type:"CHANGE_EDIT_STATUS"})
-    console.log(store1.getState());
+    
     store1.dispatch({
       type:"ADD_NEW",
       newItem :"tai nghe"
     })
-    console.log(store1.getState());
+   
     store1.dispatch({
       type:"DELETE",
       index:0
     })
-    console.log(store1.getState());
+   
     
     
     
