fa9164a9198c78a44c4634ae2bd0175c1415264c
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-25 11:36:00 +0700

    [PHAN-34] Bat dau voi Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react';
 
-function One() {
-  <div>
-     <h2> so 1 tap 1 </h2>
-     <h3> so 1 tap 2 </h3>
-     </div>
-}
 
 class App extends Component {
+  
   render() {
+
+    // var a1 = [3,5,6,7];
+    // var a2 = [...a1];
+    // console.log(a1);
+    // a2[0]=100;
+    // console.log(a2);
+    
+    var b1 = {
+      num : [34,35,36],
+      status : true
+    }
+    var b2 = {...b1,status:false};
+    console.log(b2);
+    var b3 = {...b1,num:[...b1.num,100]};
+
+    console.log(b3);
+    
+    
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <One/>
       </header>
     </div>
     );
