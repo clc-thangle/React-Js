e34657f4166e73c6ebfa487d30ed5d64f4bd9a79
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-26 17:05:07 +0700

    [PHAN-42] 123 Buoc so 2 - click vao nut lay duoc du lieu

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
 class NoteForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            noteTitle:'',
+            noteContent:''
+        }
+    }
+    
+
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        console.log(name);
+        console.log(value);    
+        this.setState({
+            [name]:value
+        });
+    }
+
+    addData  = (title,content) => {
+        var item = {};
+        item.title=title;
+        item.content=content;
+        // gui item len app de app xu li
+        console.log(item);
+        
+    }
+
     render() {
         return (
             <div className="col-4">
                 <h3>EDIT CONTENT NOTE</h3>
                 <form>
-
-                
                 <div className="form-group">
                     <label htmlFor="title">Note's title</label>
                     <input type="text" onChange={(event)=> this.isChange(event)} className="form-control" name="title" id="title" aria-describedby="helpIdTitle"  />
@@ -19,6 +46,7 @@ class NoteForm extends Component {
                     <small id="helpIdContent" className="form-text text-muted">Điền nội dung dô đaiz nữa nè </small>
                 </div>
                 <button type="reset" onClick={() => this.addData(this.state.title, this.state.content)}  className="btn btn-primary btn-block">Lưu</button></form>
+    
             </div>
         );
     }
