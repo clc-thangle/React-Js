8a99401ac6f7542bcc498c798d01eb8fbadaaffa
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-27 01:21:15 +0700

    [PHAN-44] 130 Xu ly goi du lieu ra danh sach bang redux

---
+++ src/NoteItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class NoteItem extends Component {
+    render() {
+        return (
+            <div className="card">
+            <div className="card-header" role="tab" id="note1-content">
+                <h5 className="mb-0">
+                    <a data-toggle="collapse" data-parent="#noteList" href={"#number" + this.props.i} aria-expanded="true" aria-controls="note1">
+                        
+                        {this.props.noteTitle}
+                    </a>
+                    <div className="btn-group float-right">
+                        <button className="btn btn-outline-info">Sua</button>
+                        <button className="btn btn-outline-secondary">Xoa</button>
+                    </div>
+                </h5>
+            </div>
+            <div id={"number" + this.props.i} className="collapse in" role="tabpanel" aria-labelledby="note1-content">
+                <div className="card-body">
+                    {this.props.noteContent}
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default NoteItem;
