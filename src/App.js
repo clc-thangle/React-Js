9f07a739313864965d403d482214b04a828c7324
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-26 13:23:37 +0700

    [PHAN-41] 120 Them du lieu vao firebase bang 2 cach

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {firebaseConnect} from "./firebaseConnect.js";
 import React, { Component } from 'react';
+import * as firebase from 'firebase'
 class App extends Component {
   
+  pushData  = () => {
+    var connectData = firebase.database().ref('datoForNote');
+    connectData.push({
+      tieude : "ghi chu so 3",
+      content : "noi dung ghi chu so 3"
+    })
+    console.log('ban vua them moi du lieu vao firebase');
+    
+  }
+
   render() {
     console.log(firebaseConnect);
     return (
@@ -12,6 +23,7 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo" />
 
       </header>
+      <button onClick={()=> this.pushData()}>Click de them moi bang ham push</button>
     </div>
     );
   }
