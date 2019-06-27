e254196e6905a7d0c7c294701572251e3d25a88f
Le Nho Thang <gianggiang1511@gmail.com>
2019-06-27 11:57:30 +0700

    [PHAN-46] 135 Day du lieu can sua hien thi len Form

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -39,12 +39,12 @@ class NoteForm extends Component {
                 <form>
                 <div className="form-group">
                     <label htmlFor="title">Note's title</label>
-                    <input type="text" onChange={(event)=> this.isChange(event)} className="form-control" name="title" id="title" aria-describedby="helpIdTitle"  />
+                    <input defaultValue={this.props.editItem.noteTitle} type="text" onChange={(event)=> this.isChange(event)} className="form-control" name="title" id="title" aria-describedby="helpIdTitle"  />
                     <small id="helpIdTitle" className="form-text text-muted">Điền tiêu đề dô đaiz</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="content">Note's content</label>
-                    <textarea  onChange={(event)=> this.isChange(event)} type="text" className="form-control" name="content" id="content" aria-describedby="helpIdContent" defaultValue={""} />
+                    <textarea defaultValue={this.props.editItem.noteContent} onChange={(event)=> this.isChange(event)} type="text" className="form-control" name="content" id="content" aria-describedby="helpIdContent"/>
                     <small id="helpIdContent" className="form-text text-muted">Điền nội dung dô đaiz nữa nè </small>
                 </div>
                 <button type="reset" onClick={() => this.addData(this.state.title, this.state.content)}  className="btn btn-primary btn-block">Lưu</button></form>
@@ -53,10 +53,10 @@ class NoteForm extends Component {
         );
     }
 }
-
+// props.editItem
 const mapStateToProps = (state, ownProps) => {
     return {
-        testThoi: state.testConnect
+        editItem: state.editItem
     }
 }
 
